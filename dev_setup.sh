7bfe4bc945a9f77aface8d6ea470f77e5d99230d
chLi <565353780@qq.com>
2024-04-26 18:01:15 +0800

    add mac support

--- dev_setup.sh
+++ dev_setup.sh
@@ -1,3 +1,9 @@
+if [ "$(uname)" == "Darwin" ]; then
+	pip install open3d==0.15.1
+elif [ "$(uname)" = "Linux" ]; then
+	pip install -U open3d
+fi
+
 cd ../sdf-generate/sdf_generate/Lib/Manifold
 mkdir build
 cd build
@@ -7,4 +13,4 @@ make -j
 cd ../../mesh_to_sdf
 pip install .
 
-pip install -U open3d tqdm trimesh scikit-image
+pip install -U tqdm trimesh scikit-image
