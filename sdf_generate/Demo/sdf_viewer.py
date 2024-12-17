32dc47397440ad08b998a9d6176ca7dfc03c4ddd
chLi <565353780@qq.com>
2024-12-17 14:50:07 +0800

    update viewer demo script, to render with mesh

--- sdf_generate/Demo/sdf_viewer.py
+++ sdf_generate/Demo/sdf_viewer.py
@@ -1,9 +1,23 @@
+import numpy as np
+import open3d as o3d
+
+from sdf_generate.Method.render import toNearSurfaceSDFPcd
 from sdf_generate.Module.sdf_viewer import SDFViewer
 
 
 def demo():
-    sdf_npy_file_path = "/home/chli/chLi/Dataset/SDF/ShapeNet/sdf/02691156/1026dd1b26120799107f68a9cb8e3c/models/model_normalized_obj.npy"
+    sdf_npy_file_path = "/home/chli/chLi/Dataset/Objaverse_82K/sdf_0_0025/000-001/112c059282cf4511a01fd27211edcae8.npy"
+    manifold_file_path = '/home/chli/chLi/Dataset/Objaverse_82K/manifold/000-001/112c059282cf4511a01fd27211edcae8.obj'
+
+    sdf_data = np.load(sdf_npy_file_path)
+    sdf_pcd = toNearSurfaceSDFPcd(sdf_data)
+
+    manifold_mesh = o3d.io.read_triangle_mesh(manifold_file_path)
+    o3d.visualization.draw_geometries([sdf_pcd, manifold_mesh])
+
+    return True
 
     sdf_viewer = SDFViewer()
     sdf_viewer.renderNearSurfaceSDFFile(sdf_npy_file_path)
+
     return True
